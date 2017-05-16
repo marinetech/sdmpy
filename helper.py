7336969fd1e00eb4c26eb1cd0b490a7b3de1c158
Eran Goldman-Malka <erangoldman@gmail.com>
2017-05-16 13:50:38 +0300

    add loop file

--- helper.py
+++ helper.py
@@ -1,20 +1,27 @@
 import socket
 import subprocess
 
-def reset(modemIP):
+def reset(modemIP,cmd='PHY'):
   sock = socket.create_connection((modemIP, 9200), 2)
   sock.send(b'AT?S\n')
-  if (b'PHY' not in sock.recv(1024)):
-    sock.send(b'ATP\n')
-    sock.recv(1024)
-    print ("# modem " + modemIP + " reset")
+  print("cmd : " + cmd)
+  if (cmd == 'PHY' or cmd == 'ATP'):
+    if (b'PHY' not in sock.recv(1024)):
+      sock.send(b'ATP\n')
+      sock.recv(1024)
+      print ("# modem " + modemIP + " reset to ATP")
+  else:
+    if (b'PHY' in sock.recv(1024)):
+      sock.send(b'ATA\n')
+      sock.recv(1024)
+      print ("# modem " + modemIP + " reset to ATA")
 
 def runCMD(cmd,modemID):
 	print ("# exec cmd : " + cmd)
 	shell = subprocess.Popen(["sh"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 	with open('f.tmp','w') as w:
 		w.write(cmd)
-	cmdSh = "./sdmsh " + modemID + " -f f.tmp\n"	
+	cmdSh = "./sdmsh " + modemID + " -f f.tmp\n"
 	shell.stdin.write(cmdSh.encode('ascii'))
 	shell.stdin.close()
 	for l in shell.stdout:
